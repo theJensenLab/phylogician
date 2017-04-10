6b1a83348bffcd48285c93da155f3b71f119c4de
Tony Kukavica <tonykukavica@Tonys-MBP.charter.com>
2017-04-10 02:19:59 -0700

    indentation

--- tree1.js
+++ tree1.js
@@ -1,23 +1,24 @@
 // newick tree
-    var newick = "(((A,B),C),(D,E));";
-    console.log(JSON.stringify(tnt.tree.parse_newick(newick)))
-    var tree = tnt.tree();
 
-    tree
-        .data(tnt.tree.parse_newick(newick))
-        .node_display(tree.node_display()
-            .size(4)
-            .fill("gray")
+var newick = "(((A,B),C),(D,E));";
+console.log(JSON.stringify(tnt.tree.parse_newick(newick)))
+var tree = tnt.tree();
+
+tree
+    .data(tnt.tree.parse_newick(newick))
+    .node_display(tree.node_display()
+        .size(4)
+        .fill("gray")
         )
-        .label (tnt.tree.label.text()
-            .fontsize(12)
-            .height(24)
+    .label (tnt.tree.label.text()
+    .fontsize(12)
+    .height(24)
         )
-        .layout(tnt.tree.layout.vertical()
-            .width(200)
-            .scale(false)
+    .layout(tnt.tree.layout.vertical()
+    .width(200)
+    .scale(false)
         );
-    tree(document.getElementById("treemaker"));
+tree(document.getElementById("treemaker"));
 
 function updateVertical()
 {
