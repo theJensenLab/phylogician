cb34ff85d5072289d6cbf3cde4804c800fe89be0
Tony Kukavica <tonykukavica@Tonys-MacBook-Pro.local>
2017-06-12 13:33:16 -0700

    cyan snippet commented out for future reference

--- tree1.js
+++ tree1.js
@@ -124,3 +124,17 @@ tree.on("click", function (node)
     tree.update();
 });*/
 
+/*click node=tree turns cyan
+tree.on("click", function (node)
+{
+  var root = tree.root();
+  //node.sort(function (node, node.parent()) {return 1});
+  var tempTree = root.subtree(node.get_all_leaves());
+  var nodeParent = node.parent();
+  tree.data(tempTree.data());  
+  tree.node_display(tree.node_display()
+        .size(10)
+        .fill("cyan"));
+    tree.update();
+});
+
