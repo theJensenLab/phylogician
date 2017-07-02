c4dd967ec702aaaec6b2facfd8c99ba6070709fa
Davi Ortega <ortegad@caltech.edu>
2017-07-01 23:01:48 -0700

    icon on navbar and dropdown menus

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ We will build a webApp for tree visualization.
 
 Humbly inspired by the work of others, we will use TnT, d3, express and node.
 
+## Credits:
+
+- tnt
+- d3
+- Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> 
--- src/controlBar.js
+++ src/controlBar.js
@@ -16,42 +16,131 @@ navBar.style('left', function() {
 	return '-' + (barWidth - apparent) + 'px'
 })
 
-navBar.on('click', () => {
-	console.log('click')
+let navBarBrand = d3.select('#controlBar').append('img')
+	.attr('id', 'brand')
+	.attr('src', './src/navbrand.svg')
+	.style('width', '30px')
+	.style('border-color', 'white')
+	.style('border', '5')
+
+navBarBrand.on('click', () => {
+	console.log('hey')
 	let barLeftPos = navBar.node().offsetLeft,
 		barWidth = navBar.node().offsetWidth,
 		apparent = 50,
-		newpos = '0px'
+		newpos = '-50px',
+		newText = '-',
+		duration = 1000
 
-	if (barLeftPos === 0)
+	if (barLeftPos === parseInt(newpos)) {
 		newpos = '-' + (barWidth - apparent) + 'px'
+		newText = '+'
+	}
 
-	console.log(barLeftPos)
 	navBar.transition()
-		.duration(1000)
+		.duration(duration)
 		.style('left', newpos)
+
+	navBarBrand.transition()
+		.duration(duration)
+		.text(newText)
 })
 
+function popForm(form, button) {
+	if (document.getElementById('stringInput')) {
+		document.getElementById('stringInput').style.display = 'block'
+		document.getElementById('stringSubButton').style.display = 'block'
+	}
+	else {
+		let myForm = document.createElement('input')
+		myForm.classList.add('form-control')
+		myForm.id = 'stringInput'
+		myForm.style = 'width: 300px; display: block;'
+		myForm.addEventListener('submit', function() {
+			console.log('dasad')
+			console.log(this)
+		})
+		document.body.appendChild(myForm)
 
-function division(myClass, text = '') {
-	let element = document.createElement('div')
-	if (text !== '')
-		element.innerHTML = text
-	element.classList.add(myClass)
-	return element
+		let subButton = document.createElement('button')
+		subButton.classList.add('btn')
+		subButton.type = 'button'
+		subButton.id = 'stringSubButton'
+		subButton.style = 'width: 300px; display: block;'
+		subButton.addEventListener('click', function() {
+			console.log('submitting')
+			myForm.style.display = 'none'
+			subButton.style.display = 'none'
+		})
+		document.body.appendChild(subButton)
+	}
 }
 
 
 let navBarDOM = document.getElementById('controlBar')
 
+let buttonGroup = document.createElement('div')
+buttonGroup.classList.add('btn-group')
+buttonGroup.setAttribute('data-toggle', 'buttons')
+buttonGroup.style = 'margin-left: 60px;'
+
+let inputDiv = document.createElement('div')
+inputDiv.classList.add('dropdown')
+buttonGroup.appendChild(inputDiv)
+
 let inputMenu = document.createElement('button')
-inputMenu.classList.add('btn', 'btn-primary', 'dropdown-toggle')
+inputMenu.classList.add('btn', 'dropdown-toggle')
+inputMenu.setAttribute('data-toggle', 'dropdown')
 inputMenu.type = 'button'
-inputMenu.innerHTML = 'Inputs'
+inputMenu.innerHTML = 'Load data'
+inputDiv.appendChild(inputMenu)
 
-navBarDOM.appendChild(inputMenu)
+let inputOptions = document.createElement('div')
+inputOptions.classList.add('dropdown-menu')
+inputDiv.appendChild(inputOptions)
 
-let myForm = document.createElement('input')
+let submitNwkString = document.createElement('a')
+submitNwkString.classList.add('dropdown-item')
+submitNwkString.innerHTML = 'Input newick string'
+
+inputOptions.appendChild(submitNwkString)
+
+submitNwkString.addEventListener('click', popForm)
+
+
+
+
+let submitNwkFile = document.createElement('a')
+submitNwkFile.classList.add('dropdown-item')
+submitNwkFile.innerHTML = 'Input newick file'
+inputOptions.appendChild(submitNwkFile)
+
+let operationsDiv = document.createElement('div')
+operationsDiv.classList.add('dropdown')
+buttonGroup.appendChild(operationsDiv)
+
+let operationsMenu = document.createElement('button')
+operationsMenu.classList.add('btn', 'dropdown-toggle')
+operationsMenu.setAttribute('data-toggle', 'dropdown')
+operationsMenu.type = 'button'
+operationsMenu.innerHTML = 'Tree Operations'
+operationsMenu.style = 'left: 100px;'
+operationsDiv.appendChild(operationsMenu)
+
+let operationsOptions = document.createElement('div')
+operationsOptions.classList.add('dropdown-menu')
+operationsDiv.appendChild(operationsOptions)
+
+let fit2screen = document.createElement('a')
+fit2screen.classList.add('dropdown-item')
+fit2screen.innerHTML = 'Fit to screen'
+operationsOptions.appendChild(fit2screen)
+
+
+navBarDOM.appendChild(buttonGroup)
+
+/* 
+let subButton = document.createElement('input')
 myForm.classList.add('form-control')
 myForm.id = 'userInput'
 myForm.style = 'width: 300px'
@@ -65,3 +154,4 @@ submitFileButton.classList.add('btn-success')
 submitFileButton.addEventListener('click', submitNewick)
 
 navBarDOM.appendChild(submitFileButton)
+ */
