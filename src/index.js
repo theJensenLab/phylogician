d026b6fe2a9d23b34d01cdb5e753bb3c360716d2
Tony Kukavica <kukavica@caltech.edu>
2017-08-17 16:14:02 -0700

    require tooltips.js in index

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ require('../node_modules/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.mi
 require('../node_modules/bootstrap/dist/js/bootstrap.min.js')
 require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
 require('./controlBar.js')
+require('./tooltips.js')
 
 let treeBox = document.createElement('div')
 treeBox.id = 'treeBox'
