9056efef4abbe36b33fbf1f8e440f8d5f6dc3d99
Davi Ortega <ortegad@caltech.edu>
2017-10-10 17:27:49 -0700

    adding branch lengths to exports

--- tests/tntExport.tests.js
+++ tests/tntExport.tests.js
@@ -21,5 +21,17 @@ describe('Test suit for tntExport.js', function() {
 			let sameTree = tntExport.tntObject(tree)
 			expect(sameTree).eql(treeObjOriginal)
 		})
+		it('it returns a JSON object like the input from parse_newick with branch lengths', function() {
+			let originalTree = '((C:0.5,D:0.5)1:1,(A:2,(B:0.3,X:0.4)3:3)2:4,E:0.8)R;',
+				treeObj = parser.parse_newick(originalTree)
+
+			let treeObjOriginal = JSON.parse(JSON.stringify(treeObj))
+
+			let tree = tntTree()
+			tree.data(treeObj)
+
+			let sameTree = tntExport.tntObject(tree)
+			expect(sameTree).eql(treeObjOriginal)
+		})
 	})
 })
