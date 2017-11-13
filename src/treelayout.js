985e652a3e79453b1ed41cf52e1398be4cf13bd5
Tony Kukavica <kukavica@caltech.edu>
2017-11-13 02:20:27 -0800

    commented treeLayout.js

--- src/treelayout.js
+++ src/treelayout.js
@@ -7,17 +7,33 @@ let	d3 = require('d3'),
 	currentState = 'vertical',
 	scaled = true // Default: Scaling is on.
 
-exports.updateVertical = function(tree) {
+/**
+ * Updates the back-end layout property of the tree to vertical display.
+ * 
+ * @param {any} tree The tree to be modified.
+ */
+function updateVertical(tree) {
 	tree.layout(tntTree.layout.vertical().width(window.innerWidth * 0.85))
 	currentState = 'vertical'
 }
 
-exports.updateRadial = function(tree) {
+/**
+ * Updates the back-end layout property of the tree to radial display.
+ * 
+ * @param {any} tree The tree to be modified.
+ */
+function updateRadial(tree) {
 	tree.layout(tntTree.layout.radial().width(Math.min(window.innerWidth * 0.85, window.innerHeight * 0.85)))
 	currentState = 'radial'
 }
 
-exports.updateScale = function(tree) {
+/**
+ * Toggles on/off the scaling of the tree and changes the text in the controlBar to
+ * correspond with this change. Also updates all related local variables.
+ * 
+ * @param {any} tree The tree to be modified.
+ */
+function updateScale(tree) {
 	let textUpdate = d3.select('.scalingOption')
 	if (currentState === 'vertical' && scaled === false) {
 		tree.layout(tntTree.layout.vertical().width(window.innerWidth * 0.85)
@@ -48,3 +64,8 @@ exports.updateScale = function(tree) {
 		textUpdate.text('Turn On Scaling')
 	}
 }
+
+// Exporting the following functions for use in phylogician.js:
+exports.updateVertical = updateVertical
+exports.updateRadial = updateRadial
+exports.updateScale = updateScale
